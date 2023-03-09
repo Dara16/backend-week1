c0eafa43522b6921b5ec1baca3628ae9288c5766
Dara16 <just4oludee@yahoo.com>
2023-03-09 00:06:20 -0500

    working server created

---
+++ node_project/controllers/categoryController.js
@@ -0,0 +1,37 @@
+// For '/category' endpoints
+
+const getCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'You Hit Me! Show me all the categories!'})
+}
+
+const createCategory = (req, res, next) => {
+    res
+    .status(201)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: `Created ategory with category name of
+    ${req.body.categoryName} and gender ${req.body.gender}`})
+}
+
+const putCategory = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'You Hit Me! Category updated!'})
+}
+
+const deleteCategories = (req, res, next) => {
+    res
+    .status(200)
+    .setHeader('Content-Type', 'application/json')
+    .json({ message: 'You Hit Me! Delete action completed!'})
+}
+
+module.exports = {
+    getCategories,
+    createCategory,
+    putCategory,
+    deleteCategories
+}
